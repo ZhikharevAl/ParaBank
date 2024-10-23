e20c2a93989bfeb10b86fc68ad85420b0715c96a
AlexeyZh <waltafunk@gmail.com>
2024-10-23 17:43:32 +0300

    test: added expect url

--- tests/test_parabank.py
+++ tests/test_parabank.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 
+from config.config import BASE_URL
 from pages.main_page import MainPage
 
 
@@ -7,4 +8,5 @@ def test_home_page_loading(page: Page) -> None:
     """The test checks that the main page is loaded."""
     login_page = MainPage(page)
     login_page.navigate()
-    assert login_page.is_page_loaded(), "Page is not loaded" # type: ignore
+    assert login_page.expect_url(BASE_URL), "URL does not match expected value"
+    assert login_page.is_page_loaded(), "Page content is not loaded correctly"  # type: ignore
