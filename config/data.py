e21be5a53a9660258ec9f28ea0d8a111b2c76928
AlexeyZh <waltafunk@gmail.com>
2024-10-26 00:52:37 +0300

    refactor: fix raise value error

--- config/data.py
+++ config/data.py
@@ -12,5 +12,8 @@ class Data:
     PASSWORD: str | None = os.getenv("PARABANK_PASSWORD")
 
     if not USERNAME or not PASSWORD:
-        msg: str = "Please set PARABANK_USERNAME and PARABANK_PASSWORD in .env file"
+        msg: str = (
+            "Environment variables PARABANK_USERNAME and PARABANK_PASSWORD are required. "
+            "Set them either in .env file or as environment variables"
+        )
         raise ValueError(msg)
