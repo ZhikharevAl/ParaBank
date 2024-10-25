e2d4171a5bc5eba7a1cc7f5fdb45c900f0dd04e6
AlexeyZh <waltafunk@gmail.com>
2024-10-26 01:09:54 +0300

    feat: add debug

--- config/data.py
+++ config/data.py
@@ -10,3 +10,10 @@ class Data:
 
     USERNAME: str | None = os.getenv("PARABANK_USERNAME")
     PASSWORD: str | None = os.getenv("PARABANK_PASSWORD")
+
+    print(  # noqa: T201
+        "Available environment variables:",
+        [k for k in os.environ.keys() if "PARA" in k],  # noqa: SIM118
+    )
+    print("USERNAME is set:", USERNAME is not None)  # noqa: T201
+    print("PASSWORD is set:", PASSWORD is not None)  # noqa: T201
