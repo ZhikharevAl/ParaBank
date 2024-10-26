51387c3d40fd3acc1dfb1f436ec88ce24b281804
AlexeyZh <waltafunk@gmail.com>
2024-10-26 05:42:29 +0300

    refactor: initialize data class

--- config/data.py
+++ config/data.py
@@ -8,5 +8,7 @@ load_dotenv()
 class Data:
     """The data class."""
 
-    USERNAME: str | None = os.getenv("PARABANK_USERNAME")
-    PASSWORD: str | None = os.getenv("PARABANK_PASSWORD")
+    def __init__(self) -> None:
+        """Initialize data class with credentials from environment variables."""
+        self.USERNAME: str | None = os.getenv("PARABANK_USERNAME")
+        self.PASSWORD: str | None = os.getenv("PARABANK_PASSWORD")
