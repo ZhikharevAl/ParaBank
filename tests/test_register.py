61ae0f51ca0d700f3e8ae2c42bea66ed67c5b56b
AlexeyZh <waltafunk@gmail.com>
2024-10-31 16:36:41 +0300

    test: added test_successful_registration

---
+++ tests/test_register.py
@@ -0,0 +1,38 @@
+import allure
+
+from data.user_data import UserData
+from tests.base.base_test import BaseTest
+
+
+@allure.epic("Main Application")
+@allure.feature("User Registration")
+@allure.description_html("""
+<h2>Testing User Registration Functionality</h2>
+<p>Test verifies the user registration process, including:</p>
+<ul>
+    <li>Navigation to registration page</li>
+    <li>Generating random user data</li>
+    <li>Completing registration form</li>
+    <li>Verifying successful registration</li>
+</ul>
+<p>Expected Results:</p>
+<ul>
+    <li>User should be able to register with valid data</li>
+    <li>Registration should complete successfully</li>
+</ul>
+""")
+class TestRegistration(BaseTest):
+    """Tests for user registration."""
+
+    @allure.story("Successful Registration")
+    @allure.severity(severity_level="CRITICAL")
+    def test_successful_registration(self) -> None:
+        """Test successful user registration with valid data."""
+        self.register_page.navigate()
+
+        user_data: UserData = UserData.generate_random_user()
+        self.register_page.register_new_user(user_data)  # type: ignore
+
+        assert (
+            self.register_page.is_registration_successful()
+        ), "Registration was not successful"
