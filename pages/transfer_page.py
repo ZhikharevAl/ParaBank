d6766f72638e5467198d5d9c0e668e98c7b91a85
AlexeyZh <waltafunk@gmail.com>
2024-11-13 21:49:22 +0300

    feat: added transfer page

---
+++ pages/transfer_page.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Page
+
+from config.config import TRANSFER_FUNDS_URL
+from data.amount_data import AmountData
+from pages.base_page import BasePage
+
+
+class TransferPage(BasePage):
+    """The transfer page."""
+
+    AMOUNT_INPUT = "#amount"
+    TRANSFER_BUTTON = "Transfer"
+    SHOW_RESULT = "#showResult"
+    TRANSFER_COMPLETE_TEXT = "Transfer Complete!"
+
+    def __init__(self, page: Page) -> None:
+        """The transfer page."""
+        super().__init__(page, url=TRANSFER_FUNDS_URL)
+
+    def fill_amount(self, amount_data: AmountData) -> None:
+        """Fill the amount field."""
+        self.fill_text(self.AMOUNT_INPUT, amount_data.amount)
+
+    def click_transfer(self) -> None:
+        """Click the transfer button."""
+        self.click_by_role("button", self.TRANSFER_BUTTON)  # type: ignore
+
+    def is_transfer_complete(self) -> bool:
+        """Check if the transfer is complete."""
+        return self.contains_text(self.SHOW_RESULT, self.TRANSFER_COMPLETE_TEXT)
