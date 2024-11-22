3aee0e575ea5da2fb5bf0bf7ed2fd387f2587084
AlexeyZh <waltafunk@gmail.com>
2024-11-23 00:41:18 +0300

    refactor: typing

--- pages/open_new_account_page.py
+++ pages/open_new_account_page.py
@@ -1,7 +1,14 @@
+from typing import TYPE_CHECKING
+
 import allure
 from playwright.sync_api import Page
 
 from config.config import OPEN_NEW_ACCOUNT_URL
+
+if TYPE_CHECKING:
+    from playwright.sync_api._generated import ElementHandle
+
+
 from config.data import AccountType
 from pages.base_page import BasePage
 
@@ -10,6 +17,9 @@ class OpenNewAccountPage(BasePage):  # noqa: D101
     # locators
     ACCOUNT_TYPE_SELECT = "#type"
     ACCOUNT_EXISTING_SELECT = "#fromAccountId"
+    OPEN_NEW_ACCOUNT_BUTTON = "Open New Account"
+    OPEN_ACCOUNT_RESULT = "#openAccountResult"
+    VERIFY_TEXT = "Account Opened!"
 
     def __init__(self, page: Page) -> None:
         """The open new account page."""
@@ -17,29 +27,29 @@ class OpenNewAccountPage(BasePage):  # noqa: D101
 
     def select_account_type(self, account_type: AccountType) -> None:
         """Select SAVINGS account type from dropdown."""
-        self.select_option(self.ACCOUNT_TYPE_SELECT, account_type.value)  # type: ignore
+        self.select_option(self.ACCOUNT_TYPE_SELECT, account_type.value)
 
     @allure.step("Choose existing account")
     def choose_an_existing_account(self) -> None:
         """Select first available existing account from dropdown."""
-        available_accounts = self.find_element(
+        available_accounts: list[ElementHandle] = self.find_element(
             self.ACCOUNT_EXISTING_SELECT
         ).element_handles()
         if available_accounts:
-            with allure.step("Select first available existing account {value}"):  # type: ignore
+            with allure.step("Select first available existing account {value}"):
                 self.select_option(
                     self.ACCOUNT_EXISTING_SELECT,
-                    available_accounts[0].get_attribute("value"),  # type: ignore
-                )  # type: ignore
+                    available_accounts[0].get_attribute("value"),
+                )
         else:
             error_message = "No existing accounts available to select from"
             raise ValueError(error_message)
 
     def click_button_open_new_account(self) -> None:
         """Click on Open new account button."""
-        self.click_by_role("button", "Open New Account")  # type: ignore
+        self.click_by_role("button", self.OPEN_NEW_ACCOUNT_BUTTON)
 
     @property
     def is_account_created(self) -> bool:
         """Check if account has been created."""
-        return self.contains_text("#openAccountResult", "Account Opened!")
+        return self.contains_text(self.OPEN_ACCOUNT_RESULT, self.VERIFY_TEXT)
